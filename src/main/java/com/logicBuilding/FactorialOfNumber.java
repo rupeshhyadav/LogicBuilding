9bea66192e510cdb81fcc3d93360456be8db1c43
Rupesh <rupesh0101@hotmail.com>
2025-11-20 11:40:40 +0530

    factorial of a number

---
+++ src/main/java/com/logicBuilding/FactorialOfNumber.java
@@ -0,0 +1,15 @@
+package com.logicBuilding;
+
+public class FactorialOfNumber {
+
+	public static void main(String[] args) {
+		int a=5;
+		int result =1;
+		for(int i=1;i<=a;i++) {
+			result=result*i;
+		}
+		System.out.println(result);
+
+	}
+
+}
