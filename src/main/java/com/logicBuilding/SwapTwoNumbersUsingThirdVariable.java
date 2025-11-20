13fa4edea70e7d8412ee380ee6cbeec9cd82dc30
Rupesh <rupesh0101@hotmail.com>
2025-11-20 11:34:49 +0530

    swap two numbers logic

---
+++ src/main/java/com/logicBuilding/SwapTwoNumbersUsingThirdVariable.java
@@ -0,0 +1,19 @@
+package com.logicBuilding;
+
+public class SwapTwoNumbersUsingThirdVariable {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b=20;
+		System.out.println("a is : "+a);
+		System.out.println("b is : "+b);
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a is : "+a);
+		System.out.println("b is : "+b);
+
+	}
+
+}
