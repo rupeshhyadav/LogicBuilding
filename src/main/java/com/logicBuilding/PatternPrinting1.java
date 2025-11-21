bc07b6061ec820fc21cfb107384d9172698cedce
Rupesh <rupesh0101@hotmail.com>
2025-11-21 10:14:58 +0530

    pattern printing logic

---
+++ src/main/java/com/logicBuilding/PatternPrinting1.java
@@ -0,0 +1,21 @@
+package com.logicBuilding;
+
+public class PatternPrinting1 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				if(j==1||i==n ||i==j) {
+				System.out.print(j);
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
