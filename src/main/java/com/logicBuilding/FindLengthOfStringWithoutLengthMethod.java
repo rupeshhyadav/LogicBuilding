d27984d9f0a14cfb688b02f4f03f4b74fe8928b4
Rupesh <rupesh0101@hotmail.com>
2025-11-29 09:54:14 +0530

    FindLengthOfStringWithoutLengthMethod

---
+++ src/main/java/com/logicBuilding/FindLengthOfStringWithoutLengthMethod.java
@@ -0,0 +1,15 @@
+package com.logicBuilding;
+
+public class FindLengthOfStringWithoutLengthMethod {
+
+	public static void main(String[] args) {
+		String input = "Rupesh";
+		int count = 0;
+		for (int i = 0; i < input.length(); i++) {
+			count++;
+		}
+		System.out.println(count);
+
+	}
+
+}
